3d4ecb7931bdc3233a344d1f3769169769462221
shahjalal <shahjalalkhan215@gmail.com>
2021-09-09 14:22:52 +0600

    finel project

--- index.js
+++ index.js
@@ -1,46 +1,43 @@
-const form = document.querySelector('#form-id')
-const item = document.querySelector('.item')
+
+const FormId = document.getElementById('form-id')
+const itemsValue = document.getElementById('itemsValue')
 const users = document.getElementById('users')
 const filter = document.getElementById('filter')
 
 
-form.addEventListener('submit', addItem)
+FormId.addEventListener('submit', (e) => {
+   e.preventDefault()
+  const itemsValues = itemsValue.value
+   if(itemsValue.value == ''){
 
-function addItem (e){
-e.preventDefault()
-const itemsValue = document.getElementById('itemsValue').value
-if(itemsValue == ''){
-
-}else{
+   }else{
     const li = document.createElement('li')
     li.className = 'item'
-    li.appendChild(document.createTextNode(itemsValue))
+    li.appendChild(document.createTextNode(itemsValues))
     users.appendChild(li)
     const i = document.createElement('i')
-    i.className = 'fas fa-times delete'
+    i.className = 'fas fa-times delate'
     li.appendChild(i)
-   li.addEventListener('click', (e) => {
-    if(e.target.classList.contains('delete')){
-        const items = e.target.parentElement;
-        users.removeChild(items)
-        console.log(li)
-    }
-   })
-}
-    
-}
+    itemsValue.value = ''
+    li.addEventListener('click', (e) => {
+        if(e.target.classList.contains('delate')){
+            const items = e.target.parentElement;
+           users.removeChild(items)
+        }
+    })
+   }   
+})
+
 
 filter.addEventListener('keyup', (e) => {
-    const text = e.target.value.toLowerCase();
-    const items = users.getElementsByTagName('li')
+    const text = e.target.value.toLowerCase()
+    const items = users.getElementsByTagName('li') 
     Array.from(items).forEach((item) => {
-        const itemName = item.firstChild.textContent;
-        if(itemName.toLowerCase().indexOf(text) != -1){
+        const itemContent = item.firstChild.textContent
+        if(itemContent.toLowerCase().indexOf(text) != -1){
             item.style.display = 'flex'
         }else{
             item.style.display = 'none'
         }
     })
 })
-
-
