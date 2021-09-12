939b0a87d3abb34783f68267458a3aa33bbca138
shahjalal <shahjalalkhan215@gmail.com>
2021-09-12 19:01:36 +0600

    bookList add

--- index.js
+++ index.js
@@ -1,43 +1,87 @@
+// Book Class: Represents a Book
+class Book {
+    constructor(title, author, email){
+        this.title = title,
+        this.author = author,
+        this.email = email
+    }
+}
 
-const FormId = document.getElementById('form-id')
-const itemsValue = document.getElementById('itemsValue')
-const users = document.getElementById('users')
-const filter = document.getElementById('filter')
+// UI Class: Handles Storage
 
 
-FormId.addEventListener('submit', (e) => {
-   e.preventDefault()
-  const itemsValues = itemsValue.value
-   if(itemsValue.value == ''){
+// Store Class: Handles Storage
 
-   }else{
-    const li = document.createElement('li')
-    li.className = 'item'
-    li.appendChild(document.createTextNode(itemsValues))
-    users.appendChild(li)
-    const i = document.createElement('i')
-    i.className = 'fas fa-times delate'
-    li.appendChild(i)
-    itemsValue.value = ''
-    li.addEventListener('click', (e) => {
-        if(e.target.classList.contains('delate')){
-            const items = e.target.parentElement;
-           users.removeChild(items)
+// Event: Display Books
+class UI {
+    static displayBooks(){
+      
+        const books = StoredBook()
+        books.forEach((book) => UI.addBookToList(book))
+    }
+    static alerts(massage, className){
+        const div = document.createElement('div') 
+        div.className = `alert alert-${className}`
+        div.appendChild(document.createTextNode(massage))
+        const formId = document.querySelector('#formId')
+        const container = document.querySelector('.container')
+        container.insertBefore(div, formId)
+
+        setTimeout(() => {
+            const alert = document.querySelector('.alert')
+            alert.remove()
+            
+        }, 3000)
+    }
+    static addBookToList(book){
+        const booked = document.querySelector('#book-list')
+        const row = document.createElement('tr')
+        row.innerHTML = `
+            <td>${book.title}</td>
+            <td>${book.author}</td>
+            <td>${book.email}</td>
+            <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
+        `
+        booked.appendChild(row)
+    }
+    static clearFields(){
+        document.querySelector('#title').value = ''
+    document.querySelector('#author').value = ''
+    document.querySelector('#email').value = ''
+    }
+    static deleteBook(el){
+        if(el.classList.contains('delete')){
+            el.parentElement.parentElement.remove()
         }
-    })
-   }   
+    }
+}
+
+
+document.addEventListener('DOMContentLoaded', UI.displayBooks)
+
+
+// Event: add a Book
+const formId = document.querySelector('#formId')
+formId.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const title = document.querySelector('#title').value
+    const author = document.querySelector('#author').value
+    const email = document.querySelector('#email').value
+    if(title == '' || author == ''|| email == ''){
+        UI.alerts('Please Full Fill', 'danger fs-4')
+    }else{
+        UI.alerts('Add Book Success', 'primary fs-4')
+        const book = new Book(title, author, email)
+    UI.addBookToList(book)
+    UI.clearFields()
+    
+    }
+})
+
+// Event: Remove a Book
+document.querySelector('#book-list').addEventListener('click', (e) => {
+    UI.deleteBook(e.target)
+    UI.alerts('Book Removed', 'success fs-4')
 })
 
 
-filter.addEventListener('keyup', (e) => {
-    const text = e.target.value.toLowerCase()
-    const items = users.getElementsByTagName('li') 
-    Array.from(items).forEach((item) => {
-        const itemContent = item.firstChild.textContent
-        if(itemContent.toLowerCase().indexOf(text) != -1){
-            item.style.display = 'flex'
-        }else{
-            item.style.display = 'none'
-        }
-    })
-})
