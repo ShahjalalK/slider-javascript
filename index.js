be5e3624b6d554142b6eeb4a7042c58062e5b812
shahjalal <shahjalalkhan215@gmail.com>
2021-09-15 18:48:08 +0600

    Counter App javascript

--- index.js
+++ index.js
@@ -1,23 +1,36 @@
-const loadText = document.querySelector('.loading-text')
-const bg = document.querySelector('.bg')
 
-let load = 0
+const counters = document.querySelectorAll('#count')
+const speed = 200
 
-class Loading{
-    static blurring(){
-        load++ 
-        if(load > 90){
-            clearInterval(int)
+
+counters.forEach((counter) => {
+    const upDate = () => {
+        const target = Number(counter.getAttribute('data-target'))
+        const count = Number(counter.innerText)
+        console.log(count)
+        const inc = target / speed
+        console.log(inc)
+        if(count < target){
+            counter.innerText = count + inc
+            setTimeout(upDate, 1)
+        }else{
+            counter.innerText = target 
         }
-        
-        loadText.innerHTML = `${load}%`
-        loadText.style.opacity = scale(load, 0, 100, 1, 0)
-        bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
     }
-}
+    
+    upDate()
+})
 
-const scale = (num, in_min, in_max, out_min, out_max) => {
-    return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
-}
-const int =  setInterval(Loading.blurring, 30);
+
+
+
+
+
+
+
+
+
+
+
+ 
 
